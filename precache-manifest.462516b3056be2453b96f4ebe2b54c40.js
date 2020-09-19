654b9b38db02ff49e81866f59a247209c555f28e
Jerry S Joseph <jerin.sebastian153@gmail.com>
2020-09-19 16:03:21 +0530

    Updates

---
+++ precache-manifest.462516b3056be2453b96f4ebe2b54c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17df0f8f3b0c4141a5fae930acf23883",
+    "url": "/React_tinderClone/index.html"
+  },
+  {
+    "revision": "c92ecb599d7034855d19",
+    "url": "/React_tinderClone/static/css/main.3ebeff93.chunk.css"
+  },
+  {
+    "revision": "c1a29b856a60d79609f1",
+    "url": "/React_tinderClone/static/js/2.e4e15e19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_tinderClone/static/js/2.e4e15e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92ecb599d7034855d19",
+    "url": "/React_tinderClone/static/js/main.58c98154.chunk.js"
+  },
+  {
+    "revision": "8d9b03deed369ccac2cb",
+    "url": "/React_tinderClone/static/js/runtime-main.a28b4230.js"
+  }
+]);
